676e5598d4bc1a29034e3b06c936ec091b6f45f0
Valera1978 <valera1978@tut.by>
2019-02-17 16:22:22 +0300

    fix build
    (cherry picked from commit 2d40524462011bd489cdb585674405828cd88d39)
    (cherry picked from commit 688d9b27e5c02b821a62f4a4d4b83d8ace4cafb0)

--- drivers/sensors/bmg160.c
+++ drivers/sensors/bmg160.c
@@ -82,7 +82,6 @@ struct bmg160_p {
 	int gyro_int;
 	int gyro_drdy;
 	u64 timestamp;
-        u64 old_timestamp;
 };
 
 static int bmg160_open_calibration(struct bmg160_p *);
@@ -449,38 +448,15 @@ static void bmg160_work_func(struct work_struct *work)
 	struct bmg160_p *data = container_of(work, struct bmg160_p, work);
 	struct timespec ts;
 	int time_hi, time_lo;
-	u64 delay = ktime_to_ns(data->poll_delay);
 
-	ts = ktime_to_timespec(alarm_get_elapsed_realtime());
+	ts = ktime_to_timespec(ktime_get_boottime());
 	data->timestamp = ts.tv_sec * 1000000000ULL + ts.tv_nsec;
+	time_lo = (int)(data->timestamp & TIME_LO_MASK);
+	time_hi = (int)((data->timestamp & TIME_HI_MASK) >> TIME_HI_SHIFT);
 
 	ret = bmg160_read_gyro_xyz(data, &gyro);
 	if (ret < 0)
 		return;
-
-	if (data->old_timestamp != 0 &&
-	   ((data->timestamp - data->old_timestamp) > ktime_to_ms(data->poll_delay) * 1800000LL)) {
-
-		u64 shift_timestamp = delay >> 1;
-		u64 timestamp = 0ULL;
-		//u64 diff = 0ULL;
-
-		for (timestamp = data->old_timestamp + delay; timestamp < data->timestamp - shift_timestamp; timestamp+=delay) {
-				time_hi = (int)((timestamp & TIME_HI_MASK) >> TIME_HI_SHIFT);
-				time_lo = (int)(timestamp & TIME_LO_MASK);
-
-				input_report_rel(data->input, REL_RX, gyro.x - data->caldata.x);
-				input_report_rel(data->input, REL_RY, gyro.y - data->caldata.y);
-				input_report_rel(data->input, REL_RZ, gyro.z - data->caldata.z);
-				input_report_rel(data->input, REL_X, time_hi);
-				input_report_rel(data->input, REL_Y, time_lo);
-				input_sync(data->input);
-				data->gyrodata = gyro;
-		}
-	}
-
-	time_hi = (int)((data->timestamp & TIME_HI_MASK) >> TIME_HI_SHIFT);
-	time_lo = (int)(data->timestamp & TIME_LO_MASK);
 
 	input_report_rel(data->input, REL_RX, gyro.x - data->caldata.x);
 	input_report_rel(data->input, REL_RY, gyro.y - data->caldata.y);
@@ -489,7 +465,6 @@ static void bmg160_work_func(struct work_struct *work)
 	input_report_rel(data->input, REL_Y, time_lo);
 	input_sync(data->input);
 	data->gyrodata = gyro;
-	data->old_timestamp = data->timestamp;
 }
 
 static void bmg160_set_enable(struct bmg160_p *data, int enable)
@@ -529,7 +504,6 @@ static ssize_t bmg160_enable_store(struct device *dev,
 
 	if (enable) {
 		if (pre_enable == OFF) {
-			data->old_timestamp = 0LL;
 			bmg160_open_calibration(data);
 			bmg160_set_mode(data, BMG160_MODE_NORMAL);
 			atomic_set(&data->enable, ON);
