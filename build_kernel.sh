1224f17dbea0201349c3c03570fad3afec1c6d9f
Valera1978 <valera1978@tut.by>
2019-10-28 11:27:18 +0300

    add other stuff

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,5 +7,5 @@ rm -rf output
 mkdir output
 
 #make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=lineageos_viennalteeur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output lineage_viennalteeur_defconfig
+make -C $(pwd) O=output lineage_mondrian_defconfig
 make -j16 -C $(pwd) O=output
